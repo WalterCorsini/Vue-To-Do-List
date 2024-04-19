8f01149448effccaeed092f4b55265988ffde843
WalterCorsini <corsini.walter@gmail.com>
2024-04-19 15:25:05 +0200

    add milestone 2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 const { createApp } = Vue;
 const app = createApp({
     data(){
-        
         return{
             done: "done",
+            inputUser: "",
+            newTodo:{text:"",done:true},
             todo: [
             {text: "Recruiting blog post", done: true},
             {text: "Mobile app luaunch", done: false},
@@ -12,5 +13,15 @@ const app = createApp({
             {text: "Summit update to mobile storefronts", done: true},
             ]
         };
+    },
+    methods: {
+        addTodo: function(){
+            if(this.inputUser !== ""){
+            this.newTodo.text = this.inputUser;
+            this.todo.push({...this.newTodo});
+            this.newTodo.text = "";
+            this.inputUser = "";
+            }
+        }
     },
 }).mount("#app");
